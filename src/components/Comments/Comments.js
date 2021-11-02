aec058f7be1c14085fa50e03442bd6d073a04e38
unknown <lidanyang1111@gmail.com>
2021-11-02 16:24:11 -0700

    version2

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,7 +9,7 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
-      {comments.map(comment => <Comment comment={comment} key={comment.id} />)}
+      {comments.map((comment,index) => <Comment comment={comment} key={index} />)}
     </div>
   );
 };
